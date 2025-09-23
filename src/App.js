7b25c24957b57a1766720c7f437d10f98eab8471
GhMilen <gherghemilen@gmail.com>
2025-09-23 15:26:47 +0300

    controlled elements in forms

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { getSuggestedQuery } from "@testing-library/dom";
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -26,23 +29,44 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-
+//controled elements in forms. input and select mentain theyr state in the DOM
+//controlled elements technique
+//1. useState hook
+//2 use the hook as the value of the input field
+//3 use onChange on the input to listen for the changes
 function Form() {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(1);
+
   function handeleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, amount, packed: false, id: Date.now };
+
+    setDescription("");
+    setAmount(1);
   }
 
   return (
     <form className="add-form" onSubmit={handeleSubmit}>
       <h3>What do you need for your trip</h3>
-      <select name="" id="">
+      <select
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
